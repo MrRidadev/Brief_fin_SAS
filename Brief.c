cb3aaf74ef13f0489451c53980ade0d7104d6587
ridaDev <kiratterida@gmail.com>
2024-11-22 16:23:11 +0100

    cree la fonction affichier et modifier et supprimier avec modification de code

--- Brief.c
+++ Brief.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <stdio.h> 
+
 typedef struct 
 {
     int annee;
@@ -10,29 +11,98 @@ typedef struct
 {
     char titre [100];
     char  description_de_la_tache [225];
-    char la_priorite;
+    char la_priorite [6];
      date date; //stracture imbriqué
 
 }information;
 
 //  fonctions Ajouter
+information inf;
 void ajouter(){
-    information inf;
-    printf("entrer titre:\n");
-    scanf("%s",&inf.titre);
-    printf("entrer description:\n");
-    scanf("%s",&inf.description_de_la_tache);
-    printf("entrer date annee:\n");
-    scanf("%i",&inf.date.annee);
+   
+    printf("entrer titre: ");
+    scanf("%s",inf.titre);
+    printf("  entrer description: ");
+    scanf(" %s",inf.description_de_la_tache);
+    printf("entrer la_priorite(high, low): ");
+    scanf("%s",inf.la_priorite);
+    printf("entrer date annee:");
+    scanf("%d",&inf.date.annee);
     printf("moin:");
-    scanf("%i",&inf.date.moin);
+    scanf("%d",&inf.date.moin);
     printf("jour:");
-    scanf("%i",&inf.date.jour);
+    scanf("%d",&inf.date.jour);
+    
+
+     if (inf.date.jour < 1 || inf.date.jour > 31 ||
+        inf.date.moin < 1 || inf.date.moin > 12 ||
+        inf.date.annee < 0) {
+        printf("Date invalide, veuillez reessayer.\n");
+        return;
+    }
+
+    printf("ajouter avec succes !");
+}
+// fonction afficher 
+void afficher() {
+    int choix ;
+    if (choix==1)
+    {
+         printf("Titre: %s\n", inf.titre);
+         printf("Description de la tache: %s\n", inf.description_de_la_tache);
+         printf("priorite: %s\n", inf.la_priorite);
+         printf("Date (jour/moin/annee): %d / %d / %d\n", inf.date.jour, inf.date.moin, inf.date.annee);
+    }else{
+        printf("erreur");
+    }  
+    
+   
+}
+//fonction Modifier
+
+void modifier() {
+    printf("Entrer le nouveau titre :\n ");
+    scanf("%s", inf.titre); 
+
+    printf("Entrer la nouvelle description :\n ");
+    scanf("%s", inf.description_de_la_tache); 
+
+    printf("entrer la_priorite(h pour high, l pour low):\n");
+    scanf("%c",&inf.la_priorite);
+
+    printf("entrer date annee:");
+    scanf("%d",&inf.date.annee);
+    printf("moin:");
+    scanf("%d",&inf.date.moin);
+    printf("jour:");
+    scanf("%d",&inf.date.jour);
+
+    // Vérification basique des entrées
+    if (inf.date.jour < 1 || inf.date.jour > 31 ||
+        inf.date.moin < 1 || inf.date.moin > 12 ||
+        inf.date.annee < 0) {
+        printf("Date invalide, veuillez reessayer.\n");
+        return;
+    }
+
+    printf("Tache modifiee avec succes !\n");
+}
+// fonction supprimier
+void supprimier() {
+    
+    inf.titre[0] = '\0';  
+    inf.description_de_la_tache[0] = '\0';
+    inf.la_priorite [0] = '\0';
+    inf.date.annee = 0;
+    inf.date.moin = 0;
+    inf.date.jour = 0;
+
+    printf("La tache a ete supprimee avec succes !\n");
 }
 
 int main(){
     int choix ;
-   
+    
     do
     {
         printf("\n ========= Menu =========== \n");
@@ -46,11 +116,19 @@ int main(){
         {
         case 1:ajouter();
             break;
-        
-        default:
+        case 2:afficher();
             break;
+        case 3:modifier();
+            break;
+        case 4:supprimier();
+            break;
+
+        default:
+            printf("Choix invalide, veuillez reessayer.\n");
+
+    
         }
-    } while (choix !=0);
+    } while (choix !=5);
     
 
 }
