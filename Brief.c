9d81de585c3a17155df3074d2334f95616f2eb75
ridaDev <kiratterida@gmail.com>
2024-11-26 14:58:09 +0100

    changer condition if a boucle do while

--- Brief.c
+++ Brief.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-typedef struct {
+typedef struct
+{
     int annee;
     int moin;
     int jour;
 } date;
 
-typedef struct {
+typedef struct
+{
     char titre[100];
     char description_de_la_tache[225];
     int la_priorite;
@@ -17,121 +19,130 @@ information inf[20];
 int taille = 0;
 
 // Fonction Ajouter
-void ajouter() {
+void ajouter()
+{
     printf("Entrer titre: ");
     scanf(" %[^\n]", inf[taille].titre);
     printf("Entrer description: ");
     scanf(" %[^\n]", inf[taille].description_de_la_tache);
-    
+
     do
     {
-       printf("Entrer la priorite (1 pour high, 2 pour low): ");
+        printf("Entrer la priorite (1 pour high, 2 pour low): ");
         scanf("%d", &inf[taille].la_priorite);
-    } while (inf[taille].la_priorite!=1 && inf[taille].la_priorite!=2);
-    
+    } while (inf[taille].la_priorite != 1 && inf[taille].la_priorite != 2);
 
     do
     {
         printf("Entrer date annee: ");
-    scanf("%d", &inf[taille].date.annee);
-    printf("Mois: ");
-    scanf("%d", &inf[taille].date.moin);
-    printf("Jour: ");
-    scanf("%d", &inf[taille].date.jour);
+        scanf("%d", &inf[taille].date.annee);
+        printf("Mois: ");
+        scanf("%d", &inf[taille].date.moin);
+        printf("Jour: ");
+        scanf("%d", &inf[taille].date.jour);
     } while (inf[taille].date.jour < 1 || inf[taille].date.jour > 31 ||
-        inf[taille].date.moin < 1 || inf[taille].date.moin > 12 ||
-        inf[taille].date.annee < 0);
-    
-        taille++;
-        printf("Ajoute avec succes !\n");
-    
+             inf[taille].date.moin < 1 || inf[taille].date.moin > 12 ||
+             inf[taille].date.annee < 0);
+
+    taille++;
+    printf("Ajoute avec succes !\n");
 }
 
 // Fonction Afficher
-void afficher() {
-    for (int i = 0; i < taille; i++) {
-        printf("=====La tache%d ===== \n", i + 1);
-        printf("Titre: %s\n", inf[i].titre);
-        printf("Description de la tache: %s\n", inf[i].description_de_la_tache);
-        
-        if (inf[i].la_priorite== 1)
-        {
-            printf("la priorite: high\n");
-        }else  
-        {
-            printf("la priorite: low \n");
-        }
-        
-        printf("Date (JJ/MM/AAAA): %d / %d / %d\n", inf[i].date.jour, inf[i].date.moin, inf[i].date.annee);
-    }
-}
-
-// fonction Filtrer 
-void filtrer(){
-    int p,i;
-
-do
-    {
-       printf("entrer prioriter (1 pour high ou 2 pour low) pour filtrer:\n " );
-        scanf("%d",&p);
-    } while (p!=1 && p!=2);
-
-for ( i = 0; i < taille; i++)
+void afficher()
 {
-    if (inf[i].la_priorite==p)
+    for (int i = 0; i < taille; i++)
     {
+        printf("=====La tache %d ===== \n", i + 1);
         printf("Titre: %s\n", inf[i].titre);
         printf("Description de la tache: %s\n", inf[i].description_de_la_tache);
-         if (inf[i].la_priorite== 1)
+
+        if (inf[i].la_priorite == 1)
         {
             printf("la priorite: high\n");
-        }else  
+        }
+        else
         {
             printf("la priorite: low \n");
         }
-        
-        
+
         printf("Date (JJ/MM/AAAA): %d / %d / %d\n", inf[i].date.jour, inf[i].date.moin, inf[i].date.annee);
     }
-    
 }
 
+// fonction Filtrer
+void filtrer()
+{
+    int p, i;
+
+    do
+    {
+        printf("entrer prioriter (1 pour high ou 2 pour low) pour filtrer:\n ");
+        scanf("%d", &p);
+    } while (p != 1 && p != 2);
+
+    for (i = 0; i < taille; i++)
+    {
+        if (inf[i].la_priorite == p)
+        {
+            printf("Titre: %s\n", inf[i].titre);
+            printf("Description de la tache: %s\n", inf[i].description_de_la_tache);
+            if (inf[i].la_priorite == 1)
+            {
+                printf("la priorite: high\n");
+            }
+            else
+            {
+                printf("la priorite: low \n");
+            }
+
+            printf("Date (JJ/MM/AAAA): %d / %d / %d\n", inf[i].date.jour, inf[i].date.moin, inf[i].date.annee);
+        }
+    }
 }
 
 // Fonction Modifier
-void modifier(int index) {
-    if (index >= 0 && index < taille) {
+void modifier(int index)
+{
+    if (index >= 0 && index < taille)
+    {
         printf("Entrer le nouveau titre: ");
         scanf(" %[^\n]", inf[index].titre);
         printf("Entrer la nouvelle description: ");
         scanf(" %[^\n]", inf[index].description_de_la_tache);
         printf("Entrer la priorite (1 pour high, 2 pour low): ");
         scanf("%d", &inf[index].la_priorite);
-        printf("Entrer date annee: ");
-        scanf("%d", &inf[index].date.annee);
-        printf("Mois: ");
-        scanf("%d", &inf[index].date.moin);
-        printf("Jour: ");
-        scanf("%d", &inf[index].date.jour);
 
-        if (inf[index].date.jour < 1 || inf[index].date.jour > 31 ||
-            inf[index].date.moin < 1 || inf[index].date.moin > 12 ||
-            inf[index].date.annee < 0) {
-            printf("Date invalide, veuillez reessayer.\n");
-        } else {
-            printf("Tache modifiee avec succes !\n");
-        }
-    } else {
-        printf("Index invalide !\n");
+        do
+        {
+            printf("Entrer date annee: ");
+            scanf("%d", &inf[index].date.annee);
+            printf("Mois: ");
+            scanf("%d", &inf[index].date.moin);
+            printf("Jour: ");
+            scanf("%d", &inf[index].date.jour);
+        } while ((inf[index].date.jour < 1 || inf[index].date.jour > 31) ||
+                 (inf[index].date.moin < 1 || inf[index].date.moin > 12) ||
+                 inf[index].date.annee < 0);
+        printf("Tache modifiee avec succes !\n");
+    }
+    else
+    {
+        printf("error!\n");
     }
 }
 
 // Fonction Supprimer
-void supprimier(int index) {
-    if (index < 0 || index >= taille) {
+void supprimier(int index)
+{
+    if (index < 0 || index >= taille)
+    {
         printf("Index invalide, impossible de supprimer !\n");
-    } else {
-        for (int i = index; i < taille - 1; i++) {
+    }
+    else
+    {
+        for (int i = index; i < taille - 1; i++)
+        {
             inf[i] = inf[i + 1];
         }
         taille--;
@@ -139,55 +150,70 @@ void supprimier(int index) {
     }
 }
 
-int main() {
+int main()
+{
     int choix;
     int index;
-
-    do {
+    // Menu
+    do
+    {
         printf("\n ========= Menu =========== \n");
-        printf("1 - Ajouter\n");
-        printf("2 - Afficher\n");
-        printf("3 - Modifier\n");
-        printf("4 - Supprimer\n");
-        printf("5 - Filtrer\n");
-        printf("6 - Quitter\n");
+        printf("1 - Ajouter \n");
+        printf("2 - Afficher \n");
+        printf("3 - Modifier \n");
+        printf("4 - Supprimer \n");
+        printf("5 - Filtrer \n");
+        printf("6 - Quitter \n");
         printf("Entrer le choix: ");
         scanf("%d", &choix);
 
-        switch (choix) {
+        switch (choix)
+        {
         case 1:
             ajouter();
             break;
         case 2:
             afficher();
             break;
+
         case 3:
-            printf("Entrer l'index de la tache a modifier: ");
-            scanf("%d", &index);
-            if (index < 1 || index > taille) {
-                printf("Index invalide !\n");
-            } else {
-                modifier(index - 1);
-            }
+            do
+            {
+                printf("Entrer l'index de la tache a modifier (entre 1 et %d) : ", taille);
+                scanf("%d", &index);
+
+                if (index < 1 || index > taille)
+                {
+                    printf("Index invalide ! Veuillez reessayer.\n");
+                }
+            } while (index < 1 || index > taille);
+
+            // Une fois un index valide entré
+            modifier(index - 1);
             break;
+
         case 4:
             printf("Entrer l'index de la tache a supprimer: ");
             scanf("%d", &index);
-            if (index < 1 || index > taille) {
+            if (index < 1 || index > taille)
+            {
                 printf("Index invalide !\n");
-            } else {
+            }
+            else
+            {
                 supprimier(index - 1);
             }
             break;
-        case 5:filtrer();
-        break;
+        case 5:
+            filtrer();
+            break;
         case 6:
             printf("Au revoir !\n");
             break;
         default:
             printf("Choix invalide, veuillez reessayer.\n");
         }
-    } while (choix != 7);
+    } while (choix != 6);
 
     return 0;
 }
